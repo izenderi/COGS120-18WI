f70c56236dc2d67cf8cecdb0d5f7aa2ce40fdee5
Ziliang Zhang <ziz137@ucsd.edu>
2018-02-20 11:39:53 -0800

    test case 1

--- app.js
+++ app.js
@@ -44,12 +44,16 @@ if ('development' == app.get('env')) {
 
 //Add Routes heres!!!!!
 app.get('/', index.view);
+app.get('/index', index.view);
+//app.get('/index', index.view);
 app.get('/list/:name', list.viewList);
+app.get('/list/:name', list.view)
 //app.get('/item', item.addItem);
 app.get('/login', login.view);
 app.get('/help', help.view);
 app.get('/edit', edit.view);
 app.get('/notes', notes.view);
+app.get('/indexLogged', index.logged);
 // Example route
 // app.get('/users', user.list);
 
--- routes/list.js
+++ routes/list.js
@@ -9,3 +9,8 @@ exports.viewList = function(req, res) { 
   });
 };
 
+exports.view = function(req, res) { 
+  // controller code goes here 
+  console.log(data.item[0]);
+  res.render('list', data);
+};
